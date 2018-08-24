7dce2c056d38b1a5565e965185087407ebb3dd84
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2018-08-24 23:45:55 +0700

    implement store and interact with metamask

--- src/views/CasinoDapp.vue
+++ src/views/CasinoDapp.vue
@@ -8,6 +8,10 @@ export default {
   name: 'casino-dapp',
   components: {
     HelloMetamask
+  },
+  beforeCreate () {
+    console.log('registerWeb3 Action dispatched from casino-dapp.vue')
+    this.$store.dispatch('registerWeb3')
   }
 }
 </script>
